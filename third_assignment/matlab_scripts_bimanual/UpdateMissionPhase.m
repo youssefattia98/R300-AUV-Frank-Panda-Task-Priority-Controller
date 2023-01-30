0efd3dc56b44cda7f4725b52d1fb6744b3a406f7
jek97 <102675914+jek97@users.noreply.github.com>
2023-01-30 17:58:41 +0100

    Add files via upload

---
+++ third_assignment/matlab_scripts_bimanual/UpdateMissionPhase.m
@@ -0,0 +1,21 @@
+function [pandaArm, mission] = UpdateMissionPhase(pandaArm, mission)    
+        switch mission.phase
+            case 1  %Go To Grasping Points
+                if (norm(pandaArm.ArmL.w_rho_Leeg) <= 0.1 && norm(pandaArm.ArmL.w_dist_Leeg) <= 0.1 && norm(pandaArm.ArmR.w_rho_Reeg) <= 0.1 && norm(pandaArm.ArmR.w_dist_Reeg) <= 0.1)
+                    mission.phase = 2;
+                    mission.previous_action = mission.current_action;
+                    mission.current_action = "grasping";
+                    mission.phase_time = 0;
+                end
+            case 2 % Cooperative Manipulation Start
+%                 if (norm(pandaArm.ArmL.q_dot) < 0.1 && norm(pandaArm.ArmR.q_dot))
+%                     mission.phase = 3;
+%                     mission.previous_action = mission.current_action;
+%                     mission.current_action = "finish";
+%                     mission.phase_time = 0;
+%                 end
+            case 3 % Finish motion
+                     
+        end
+end
+
